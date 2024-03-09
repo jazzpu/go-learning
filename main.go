8a69e1789182c064281fc65f2636feb1031f29d8
jazzpu <144272963+jazzpu@users.noreply.github.com>
2024-03-09 15:01:16 +0700

    ending day1

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 )
 
 func main() {
+
 	fmt.Printf("HelloWorld!\n")
 	// new router is to understand which handler to execute or call
 	r := mux.NewRouter()
@@ -20,7 +21,7 @@ func main() {
 	// when running the browser you must type in localhost:3000/mainfile to run the correct server
 	r.HandleFunc("/mainpage", handlers.IndexPage)
 	// Create a new handler, make this handler return text saying "Hello from Jazz!, connect this handler to url /about"
-	r.HandleFunc("/about", handlers.IndexPage2)
+	r.HandleFunc("/users/{username}", handlers.UserPage)
 	// start server by putting the IP of the server usually its like "google.com" but if it's a local host then you can use port
 	http.ListenAndServe(":3000", r)
 
